82b499e541c4534b40a6ca28337cf109b2def4c8
Jobin Johnson <jobinrjohnson@gmail.com>
2020-10-06 10:13:23 +0530

    new token

--- config/vimeo.go
+++ config/vimeo.go
@@ -3,5 +3,5 @@ package config
 // TODO move to some env based thing. Fine for now.
 var (
 	// VIMEO_OAuthToken vimeo Oauth token
-	VIMEO_OAuthToken = "67847279bec8a5f6d34b959136dfac8c"
+	VIMEO_OAuthToken = "ef99c2e4eb68eebca1f4fce3c62742b6"
 )
